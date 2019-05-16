9147abb345ef6b0a33a836e62f17eb944082a873
Joshua Freislich <joshuafreislich@josh.local>
2019-05-16 13:41:58 +1000

    test: match a large investor to multiple smaller loans

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -14,7 +14,7 @@ it('makeBook can match a large investor to a single smaller loan', () => {
 it('makeBook can match a large investor to multiple smaller loans', () => {
   const loans = [{id: 0, amount: 1e6, underwritten: 0}, {id: 1, amount: 1e5, underwritten: 0}, {id: 2, amount: 1e5, underwritten: 0}];
   const investors = [{id: 0, amount: 1.5e6, invested: 0}];
-  const expectedBook = [{loanId: 0, investorId: 0, investedAmount: 1e6}, {loanId: 1, investorId: 0, investedAmount: 1e5}, {loanId: 1, investorId: 0, investedAmount: 1e5}];
+  const expectedBook = [{loanId: 0, investorId: 0, investedAmount: 1e6}, {loanId: 1, investorId: 0, investedAmount: 1e5}, {loanId: 2, investorId: 0, investedAmount: 1e5}];
   const result = makeBook(loans, investors);
 
   expect(result).toMatchObject(expectedBook);
--- src/util.js
+++ src/util.js
@@ -1,23 +1,21 @@
 export const makeBook = (loans, investors) => {
   const book = [];
 
-  loans.map( (loan, index) => {
-    const investor = investors.shift();
-    const investedAmount = Math.min(loan.amount, investor.amount);
-    book.push({
-      loanId    : loan.id,
-      investorId: investor.id,
-      investedAmount
-    })
-    investor.invested += investedAmount;
-    loan.underwritten += investedAmount;
-  });
+  for(var loan of loans) {
+    if (loan.underwritten < loan.amount) {
+      for(var investor of investors) {
+
+        const investedAmount = Math.min(loan.amount, investor.amount);
+        book.push({
+          loanId    : loan.id,
+          investorId: investor.id,
+          investedAmount
+        })
+        investor.invested += investedAmount;
+        loan.underwritten += investedAmount;
+
+      }
+    }
+  }
   return book;
 }
-
-//
-// const findInvestor = (loans, investors) => {
-//   while (loan.underwritten < loan.amount) {
-//
-//   }
-// }
