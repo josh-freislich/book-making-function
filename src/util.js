007117c8e69daaa359d37e3713164cc62d197589
Joshua Freislich <joshuafreislich@josh.local>
2019-05-16 13:32:46 +1000

    test passes with refactor

--- src/util.js
+++ src/util.js
@@ -1,4 +1,23 @@
 export const makeBook = (loans, investors) => {
+  const book = [];
 
-  return [{loanId: 0, investorId: 0, investedAmount: 1e6}];
+  loans.map( (loan, index) => {
+    const investor = investors.shift();
+    var investedAmount = Math.min(loan.amount, investor.amount);
+    book.push({
+      loanId    : loan.id,
+      investorId: investor.id,
+      investedAmount
+    })
+    investor.invested += investedAmount;
+    loan.underwritten += investedAmount;
+  });
+  return book;
 }
+
+//
+// const findInvestor = (loans, investors) => {
+//   while (loan.underwritten < loan.amount) {
+//
+//   }
+// }
